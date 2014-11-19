efd2e0374d612aa9ba5b2150057c5b7e41982afb
LindenY <LindenY@users.noreply.github.com>
2014-11-19 09:42:12 -0500

    Update InstructionResolver.java

--- src/main/java/ca/uwaterloo/asw/InstructionResolver.java
+++ src/main/java/ca/uwaterloo/asw/InstructionResolver.java
@@ -2,8 +2,10 @@ package ca.uwaterloo.asw;
 
 public interface InstructionResolver {
 
-	public void registerInstruction(String[] requiredNames, Class instructionClass);
+	public void registerInstruction(String[] requiredNames, Class<Instruction> instructionClass);
 	public Instruction resolveInstruction();
 	public Instruction getInstruction(String[] dataNodeNames);
 	
+	public void beforInstructionExecution(Instruction instruction);
+	public void afterInstructionExecution(Instruction instruction);
 }
