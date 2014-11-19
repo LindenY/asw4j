362a29a603a68e252b83de626fa52f6d54fc88c0
LindenY <LindenY@users.noreply.github.com>
2014-11-19 17:06:39 -0500

    Update DataNodeStore.java

--- src/main/java/ca/uwaterloo/asw/DataNodeStore.java
+++ src/main/java/ca/uwaterloo/asw/DataNodeStore.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 public interface DataNodeStore <C extends Collection<DataNode>> {
 
 	public void add(DataNode dataNode);
+	public void addAll(Collection<DataNode> dataNodes);
 	public DataNode get(String name, DataNode.STAGE type);
 
 	public C getAllDataNodesWithStage(DataNode.STAGE stage);
