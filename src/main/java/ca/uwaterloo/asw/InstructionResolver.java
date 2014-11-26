9983b2c5b827bb4d3027fc280197d12cc208a137
LindenY <LindenY@users.noreply.github.com>
2014-11-26 17:09:53 -0500

    Update InstructionResolver.java

--- src/main/java/ca/uwaterloo/asw/InstructionResolver.java
+++ src/main/java/ca/uwaterloo/asw/InstructionResolver.java
@@ -2,11 +2,31 @@ package ca.uwaterloo.asw;
 
 public interface InstructionResolver {
 
-	public void registerInstruction(String[] requiredNames,  Class instructionClass);
+	public void register(String[] requiredDataNames,
+			Class<?>[] requiredDataTypes, String producedDataName,
+			Class<?> producedDataType,
+			Class<? extends Instruction<?, ?>> instructionClass);
+
+	public void register(Class<?>[] requiredDataTypes,
+			Class<?> producedDataType,
+			Class<? extends Instruction<?, ?>> instructionClass);
+
+	public void register(String[] requiredDataNames, String producedDataName,
+			Class<? extends Instruction<?, ?>> instructionClass);
+
+	public void register(String[] requiredDataNames,
+			Class<? extends Instruction<?, ?>> instructionClass);
+
+	public void register(String producedDataName,
+			Class<? extends Instruction<?, ?>> instructionClass);
+
+	public void register(Class<? extends Instruction<?, ?>> instructionClass);
+
 	public int numberOfRegisteredInstruction();
-	public Instruction resolveInstruction();
-	public Instruction getInstruction(String[] dataNodeNames);
-	
-	public void beforInstructionExecution(Instruction instruction);
-	public void afterInstructionExecution(Instruction instruction);
+
+	public Instruction<?, ?> resolveInstruction();
+
+	public void beforInstructionExecution(Instruction<?, ?> instruction);
+
+	public void afterInstructionExecution(Instruction<?, ?> instruction);
 }
