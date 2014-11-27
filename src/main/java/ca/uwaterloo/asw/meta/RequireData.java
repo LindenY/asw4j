ebbfa1298e8cb3de30d00bd77305b72cbad54df2
LindenY <LindenY@users.noreply.github.com>
2014-11-27 17:24:34 -0500

    Update RequireData.java

--- src/main/java/ca/uwaterloo/asw/meta/RequireData.java
+++ src/main/java/ca/uwaterloo/asw/meta/RequireData.java
@@ -1,7 +1,14 @@
 package ca.uwaterloo.asw.meta;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value = {ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface RequireData {
 	
 	String[] names();
-	Class<?> types();
+	Class<?>[] types() default {Object.class};
 }
