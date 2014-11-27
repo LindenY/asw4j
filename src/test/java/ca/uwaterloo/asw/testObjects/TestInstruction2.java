ad4096d5e806a0b9698e3938adb392a20257656a
LindenY <LindenY@users.noreply.github.com>
2014-11-27 09:55:52 -0500

    Create TestInstruction2.java

---
+++ src/test/java/ca/uwaterloo/asw/testObjects/TestInstruction2.java
@@ -0,0 +1,31 @@
+package ca.uwaterloo.asw.testObjects;
+
+import ca.uwaterloo.asw.Instruction;
+import ca.uwaterloo.asw.ToolResolver;
+
+public class TestInstruction2 extends Instruction<String, String> {
+
+	public TestInstruction2(ToolResolver toolResolver) {
+		super(toolResolver);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void preExecution() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String execute(String requiredData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void postExecution() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
