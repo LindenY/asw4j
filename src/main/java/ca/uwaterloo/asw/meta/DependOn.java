c1dd91ad9b0cec7e789a13c6d1136eb95fdee688
LindenY <LindenY@users.noreply.github.com>
2014-11-27 17:23:54 -0500

    Update DependOn.java

--- src/main/java/ca/uwaterloo/asw/meta/DependOn.java
+++ src/main/java/ca/uwaterloo/asw/meta/DependOn.java
@@ -1,8 +1,16 @@
 package ca.uwaterloo.asw.meta;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import ca.uwaterloo.asw.Instruction;
 
+@Target(value = {ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface DependOn {
 
-	Class<? extends Instruction<?, ?>>[] instruction();
+	Class<? extends Instruction<?, ?>>[] instructions();
+	boolean async() default true;
 }
