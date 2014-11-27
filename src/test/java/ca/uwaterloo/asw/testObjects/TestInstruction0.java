db09361eb275f29ba30b5f6ed3d8d66f1839af20
LindenY <LindenY@users.noreply.github.com>
2014-11-27 09:55:17 -0500

    Create TestInstruction0.java

---
+++ src/test/java/ca/uwaterloo/asw/testObjects/TestInstruction0.java
@@ -0,0 +1,33 @@
+package ca.uwaterloo.asw.testObjects;
+
+import java.util.Date;
+
+import ca.uwaterloo.asw.Instruction;
+import ca.uwaterloo.asw.ToolResolver;
+
+public class TestInstrucion0 extends Instruction<Date, String> {
+
+	public TestInstrucion0(ToolResolver toolResolver) {
+		super(toolResolver);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void preExecution() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String execute(Date requiredData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void postExecution() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
