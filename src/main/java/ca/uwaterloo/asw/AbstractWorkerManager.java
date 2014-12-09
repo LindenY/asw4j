dd4ce3bee5dfb26aed017932a472e12258fc8981
LindenY <LindenY@users.noreply.github.com>
2014-12-09 17:07:41 -0500

    Update AbstractWorkerManager.java

--- src/main/java/ca/uwaterloo/asw/AbstractWorkerManager.java
+++ src/main/java/ca/uwaterloo/asw/AbstractWorkerManager.java
@@ -1,11 +1,17 @@
 package ca.uwaterloo.asw;
 
-public abstract class AbstractWorkerManager<T> implements WorkerManager<T> {
+import java.lang.reflect.Type;
 
-	protected DataStore dataStore;
-	protected InstructionResolver instructionResolver;
-	
-	public AbstractWorkerManager(DataStore dataStore, InstructionResolver instructionResolver) {
+import ca.uwaterloo.asw.reflection.TypeToken;
+
+public abstract class AbstractWorkerManager<T> implements WorkerManager<T>,
+		InstructionClassRegister, DataManipulationRegister {
+
+	protected AbstractDataStore dataStore;
+	protected AbstractInstructionResolver instructionResolver;
+
+	public AbstractWorkerManager(AbstractDataStore dataStore,
+			AbstractInstructionResolver instructionResolver) {
 		this.dataStore = dataStore;
 		this.instructionResolver = instructionResolver;
 	}
@@ -14,20 +20,64 @@ public abstract class AbstractWorkerManager<T> implements WorkerManager<T> {
 		return dataStore;
 	}
 
-	public void setDataStore(DataStore dataStore) {
-		this.dataStore = dataStore;
-	}
-
 	public InstructionResolver getInstructionResolver() {
 		return instructionResolver;
 	}
 
-	public void setInstructionResolver(InstructionResolver instructionResolver) {
+	public void setDataStore(AbstractDataStore dataStore) {
+		this.dataStore = dataStore;
+	}
+
+	public void setInstructionResolver(
+			AbstractInstructionResolver instructionResolver) {
 		this.instructionResolver = instructionResolver;
 	}
-	
-	public void registerInstruction(Class<? extends Instruction<?, ?>> instructionClass) {
-		instructionResolver.register(instructionClass);
+
+	public <T> void registerBalancer(TypeToken<T> typeToken,
+			Balancer<T> balancer) {
+		dataStore.registerBalancer(typeToken, balancer);
 	}
-	
+
+	public <T> void registerCombiner(TypeToken<T> typeToken,
+			Combiner<T> combiner) {
+		dataStore.registerCombiner(typeToken, combiner);
+	}
+
+	public void registerInstructionClass(
+			Class<? extends Instruction<?, ?>> instructionClass) {
+		instructionResolver.registerInstructionClass(instructionClass);
+	}
+
+	public void registerInstructionClass(
+			Class<? extends Instruction<?, ?>> instructionClass,
+			String produceDataName) {
+		instructionResolver.registerInstructionClass(instructionClass,
+				produceDataName);
+	}
+
+	public void registerInstructionClass(
+			Class<? extends Instruction<?, ?>> instructionClass,
+			String[] requireDataNames, Type[] requireDataTypes) {
+		instructionResolver.registerInstructionClass(instructionClass,
+				requireDataNames, requireDataTypes);
+	}
+
+	public void registerInstructionClass(
+			Class<? extends Instruction<?, ?>> instructionClass,
+			String[] requireDataNames, Type[] requireDataTypes,
+			String produceDataName) {
+		instructionResolver.registerInstructionClass(instructionClass,
+				requireDataNames, requireDataTypes, produceDataName);
+	}
+
+	public void registerInstructionClass(
+			Class<? extends Instruction<?, ?>> instructionClass,
+			String[] requireDataNames, Type[] requireDataTypes,
+			String produceDataName,
+			Class<? extends Instruction<?, ?>>[] dependencies,
+			boolean supportSingleton, boolean supportAsync) {
+		instructionResolver.registerInstructionClass(instructionClass,
+				requireDataNames, requireDataTypes, produceDataName);
+	}
+
 }
