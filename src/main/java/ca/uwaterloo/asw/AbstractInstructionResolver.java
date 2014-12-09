7c953c696328bb7cbcf141b43d0ccd473e3a949f
LindenY <LindenY@users.noreply.github.com>
2014-12-09 17:06:58 -0500

    Update AbstractInstructionResolver.java

--- src/main/java/ca/uwaterloo/asw/AbstractInstructionResolver.java
+++ src/main/java/ca/uwaterloo/asw/AbstractInstructionResolver.java
@@ -1,37 +1,41 @@
 package ca.uwaterloo.asw;
 
 public abstract class AbstractInstructionResolver implements
-		InstructionResolver {
+		InstructionResolver, InstructionClassRegister {
 
 	protected ToolResolver toolResolver;
 	protected DataStore dataStore;
 
+	public AbstractInstructionResolver(DataStore dataStore) {
+		this(null, dataStore);
+	}
+	
 	public AbstractInstructionResolver(ToolResolver toolResolver,
 			DataStore dataStore) {
 		this.toolResolver = toolResolver;
 		this.dataStore = dataStore;
 	}
 
-	public DataStore getDataStore() {
-		return dataStore;
+	public void afterInstructionExecution(Instruction<?, ?> instruction, Throwable throwed) {
 	}
 
-	public void setDataStore(DataStore dataStore) {
-		this.dataStore = dataStore;
+	public void beforInstructionExecution(Instruction<?, ?> instruction) {
+	}
+
+	public DataStore getDataStore() {
+		return dataStore;
 	}
 
 	public ToolResolver getToolResolver() {
 		return toolResolver;
 	}
 
+	public void setDataStore(DataStore dataStore) {
+		this.dataStore = dataStore;
+	}
+
 	public void setToolResolver(ToolResolver toolResolver) {
 		this.toolResolver = toolResolver;
-	}
-
-	public void beforInstructionExecution(Instruction<?, ?> instruction) {
-	}
-
-	public void afterInstructionExecution(Instruction<?, ?> instruction) {
 	}
 }
 
