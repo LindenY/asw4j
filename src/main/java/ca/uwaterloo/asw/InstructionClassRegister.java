6c12b1fb1ca8cd34f5dc49c1aa6a73b947bace17
LindenY <LindenY@users.noreply.github.com>
2014-12-09 17:10:36 -0500

    Create InstructionClassRegister.java

---
+++ src/main/java/ca/uwaterloo/asw/InstructionClassRegister.java
@@ -0,0 +1,31 @@
+package ca.uwaterloo.asw;
+
+import java.lang.reflect.Type;
+
+public interface InstructionClassRegister {
+	
+	public int numberOfRegisteredInstruction();
+
+	public void registerInstructionClass(
+			Class<? extends Instruction<?, ?>> instructionClass);
+
+	public void registerInstructionClass(
+			Class<? extends Instruction<?, ?>> instructionClass,
+			String produceDataName);
+
+	public void registerInstructionClass(
+			Class<? extends Instruction<?, ?>> instructionClass,
+			String[] requireDataNames, Type[] requireDataTypes);
+
+	public void registerInstructionClass(
+			Class<? extends Instruction<?, ?>> instructionClass,
+			String[] requireDataNames, Type[] requireDataTypes,
+			String produceDataName);
+
+	public void registerInstructionClass(
+			Class<? extends Instruction<?, ?>> instructionClass,
+			String[] requireDataNames, Type[] requireDataTypes,
+			String produceDataName,
+			boolean supportSingleton);
+	
+}
