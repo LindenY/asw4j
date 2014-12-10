932914cd4fa2235a7c85fcb6fa4d1fa48ba9aa23
LindenY <LindenY@users.noreply.github.com>
2014-12-10 11:58:22 -0500

    Create InstructionClassNodeTest.java

---
+++ src/test/java/ca/uwaterloo/asw4j/unitTest/InstructionClassNodeTest.java
@@ -0,0 +1,41 @@
+package ca.uwaterlo.asw4j.unitTest;
+
+import org.junit.Test;
+
+public class InstructionClassNodeTest {
+	
+	@Test
+	public void testConstructorWithLegalParameters() {
+		
+	}
+	
+	@Test
+	public void testConstructorWithIllegalParameters() {
+		
+	}
+
+	@Test
+	public void testGetRequireDatas() {
+		
+	}
+	
+	@Test
+	public void testGetProduceData() {
+	
+	}
+	
+	@Test
+	public void testGetInstanceOfInstructionWithToolResolver() {
+		
+	}
+	
+	@Test
+	public void testGetInstanceOfInstructionWithoutToolResolver() {
+		
+	}
+	
+	@Test
+	public void testNumOfInstructionIssued() {
+		
+	}
+ }
