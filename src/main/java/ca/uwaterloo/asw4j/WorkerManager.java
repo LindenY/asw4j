96bb56ff145acf64b0f7856e0d34e9f77aa75684
LindenY <LindenY@users.noreply.github.com>
2014-12-11 17:00:34 -0500

    Update WorkerManager.java

--- src/main/java/ca/uwaterloo/asw4j/WorkerManager.java
+++ src/main/java/ca/uwaterloo/asw4j/WorkerManager.java
@@ -1,10 +1,11 @@
 package ca.uwaterloo.asw4j;
 
+import java.lang.reflect.Type;
 import java.util.concurrent.Future;
 
-public interface WorkerManager<T> {
+public interface WorkerManager {
 	
-	public Future<T> asyncStart();
+	public STATE getState();
 	
 	public void awaitShutDown(int timeOut) throws InterruptedException;
 	
@@ -12,6 +13,50 @@ public interface WorkerManager<T> {
 	
 	public void shutDownNow();
 	
-	public T start();
+	public <T> Future<T> asyncStart(Class<T> type, String name);
+	
+	public <T> T start(Class<T> type, String name);
+	
+	public void setExceptionHandlePolicy(ExceptionHandlePolicy policy);
+	
+	public ExceptionHandlePolicy getExceptionHandlePolicy();
+	
+	
+	public int numberOfRegisteredInstruction();
 
+	public void registerInstructionClass(
+			Class<? extends Instruction<?, ?>> instructionClass);
+
+	public void registerInstructionClass(
+			Class<? extends Instruction<?, ?>> instructionClass,
+			String produceDataName);
+
+	public void registerInstructionClass(
+			Class<? extends Instruction<?, ?>> instructionClass,
+			String[] requireDataNames, Type[] requireDataTypes);
+
+	public void registerInstructionClass(
+			Class<? extends Instruction<?, ?>> instructionClass,
+			String[] requireDataNames, Type[] requireDataTypes,
+			String produceDataName);
+
+	public void registerInstructionClass(
+			Class<? extends Instruction<?, ?>> instructionClass,
+			String[] requireDataNames, Type[] requireDataTypes,
+			String produceDataName,
+			boolean supportSingleton);
+
+	
+	public static enum ExceptionHandlePolicy {
+		BlockInstructionAndRun,
+		BlockInstructionPrintTraceAndRun,
+		Terminate
+	}
+	
+	public static enum STATE {
+		Ready,
+		Canceled,
+		TerminatedByException,
+		Finished
+	}
 }
