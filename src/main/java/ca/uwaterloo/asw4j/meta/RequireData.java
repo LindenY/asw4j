18edb6a4b9b90b962ea759596c119b56e73f6168
Desmond Lin <LindenY@users.noreply.github.com>
2014-12-16 14:53:26 -0500

    Adding Comments

--- src/main/java/ca/uwaterloo/asw4j/meta/RequireData.java
+++ src/main/java/ca/uwaterloo/asw4j/meta/RequireData.java
@@ -1,14 +1,38 @@
 package ca.uwaterloo.asw4j.meta;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(value = {ElementType.TYPE})
+/**
+ * <p>
+ * Provide require data names and types about {@link Instruction} classes.
+ * </p>
+ * <p>
+ * This {@link Annotation} is used by {@link InstructionResolver}.
+ * </p>
+ * 
+ * @author Desmond Lin
+ * @since 1.0.0
+ */
+@Documented
+@Target(value = { ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RequireData {
-	
+
+	/**
+	 * Get the require data names of the annotated {@link Instruction}.
+	 * 
+	 * @return The require data names of the annotated {@link Instruction}.
+	 */
 	String[] names();
-	Class<?>[] types() default {Object.class};
+
+	/**
+	 * Get the require data types of the annotated {@link Instruction}.
+	 * 
+	 * @return The require data types of annotated {@link Instruction}.
+	 */
+	Class<?>[] types() default { Object.class };
 }
