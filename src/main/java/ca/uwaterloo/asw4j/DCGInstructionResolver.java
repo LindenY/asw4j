260e903853712e7cbfcaa4cc2972cddccc9381a0
DesLin <desmond.zq.lin@gmail.com>
2014-12-23 21:28:34 -0500

    Updated DCGInstructionResolver

--- src/main/java/ca/uwaterloo/asw4j/DCGInstructionResolver.java
+++ src/main/java/ca/uwaterloo/asw4j/DCGInstructionResolver.java
@@ -3,6 +3,8 @@ package ca.uwaterloo.asw4j;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Type;
 
+import ca.uwaterloo.asw4j.reflection.TypeToken;
+
 public class DCGInstructionResolver extends AbstractInstructionResolver {
 
 	public DCGInstructionResolver(DataStore dataStore,
@@ -60,7 +62,13 @@ public class DCGInstructionResolver extends AbstractInstructionResolver {
 			Throwable throwed) {
 		Object result = instruction.getResult();
 		if (result instanceof DataNode) {
+			DataNode rdn = (DataNode) result;
 			
+			for (TypeToken<?> tk : rdn.keySet()) {
+				dataStore.addAll(rdn.getAll(tk), tk.getName());
+			}
+		} else {
+			super.afterInstructionExecution(instruction, throwed);
 		}
 	}
 	
